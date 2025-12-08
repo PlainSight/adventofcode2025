62189ea0da8be159c4ee88a233b5d345fc4a79d4
Alex <alexander.ogier@gmail.com>
2025-12-08 20:37:34 +1300

    more cleanup

--- day8/part2.js
+++ day8/part2.js
@@ -33,18 +33,18 @@ for(var j = 0; j < input.length; j++) {
     connections[j] = { parent: j, size: 1 };
 }
 
-function link(first, second) {
-    var firstObj = connections[first];
-    while (first != firstObj.parent) {
-        first = firstObj.parent;
-        firstObj = connections[first];
+function root(id) {
+    var o = connections[id];
+    while (id != o.parent) {
+        id = o.parent;
+        o = connections[id];
     }
+    return o;
+}
 
-    var secondObj = connections[second];
-    while (second != secondObj.parent) {
-        second = secondObj.parent;
-        secondObj = connections[second];
-    }
+function link(a, b) {
+    var firstObj = root(a);
+    var secondObj = root(b);
 
     if (firstObj.parent != secondObj.parent) {
         if (firstObj.size > secondObj.size) {
@@ -57,21 +57,9 @@ function link(first, second) {
     }
 }
 
-function findUnifyingSet() {
-    var id = 0;
-    var root = connections[0];
-
-    while(id != root.parent) {
-        id = root.parent;
-        root = connections[root.parent];
-    }
-
-    return root;
-}
-
 var d = 0;
 
-while (findUnifyingSet().size < input.length) {
+while (root(0).size < input.length) {
     var c1 = distances[d].j1;
     var c2 = distances[d].j2;
 
