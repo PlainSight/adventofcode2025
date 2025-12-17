8f4573fc8a4c57ccee7272f56fcd653fdf1eb723
Alex <alexander.ogier@gmail.com>
2025-12-18 01:35:10 +1300

    get a more optimised but still too slow solution working

--- day10/part2.js
+++ day10/part2.js
@@ -23,19 +23,30 @@ problems.forEach(p => {
     // for each value we can calculate the maximum number of presses for a set of buttons
 });
 
-function remainderWillCap(i, pos, pushes) {
-    problems[i].related[pos].forEach(r => [
-        // if buttons are pushed which are related to this position then we must check if is possible to get the counter to the correct position without overflowing the related positions
+function shrink(r1, rs, cap) {
+    // rs is the other ranges
+    // shrink r1 contingent on r2 and cap
 
-    ])
+    var r2 = [
+        rs.reduce((a, c) => a + c[0], 0), 
+        rs.reduce((a, c) => a + c[1], 0)
+    ];
+
+    // r1's min increases by cap - r2's max (but not beyond it's existing max)
+    // r1's max reduces by r2's min
+    var ret = [
+        Math.max(r1[0], cap-r2[1]),
+        Math.min(r1[1], cap-r2[0])
+    ];
+
+    var changed = r1[0] != ret[0] || r1[1] != ret[1];
+
+    return [ret, changed];
 }
-
-// need to do some kind of n dimensional path finding or something 
-// actually maybe a big simultaneous equation
 
 function value(i, pushes) {
     return problems[i].joltages.map((_, j) => {
-        return problems[i].wirings.reduce((a, w, wi) => a + (w.some(ww => ww == j) ? (pushes[wi] || 0) : 0), 0);
+        return problems[i].wirings.reduce((a, w, wi) => a + (w.some(ww => ww == j) ? (pushes[wi][0] || 0) : 0), 0);
     });
 }
 
@@ -46,114 +57,116 @@ function distance(i, pushes) {
     }, 0);
 }
 
-function pushCount(pushes) {
-    return pushes.reduce((a, c) => a+c, 0);
+var cache = {};
+var best = {};
+function key(buttons) {
+    return buttons.map(b => b.join(',')).join(':');
 }
 
-function clone(p) {
-    return p.map(n => n);
-}
+function search(i, buttons) {
+    var newButtons = calculateRanges(i, buttons);
 
-function search(i) {
-    var seen = {};
+    var k = key(newButtons);
+    if (cache[k]) {
+        return Number.MAX_SAFE_INTEGER;
+    }
+    cache[k] = true;
 
-    function key(n) {
-        return n.join(',');
+    if (newButtons.some(b => b[0] > b[1])) {
+        return Number.MAX_SAFE_INTEGER;
     }
 
-    var heap = [{
-        pushes: problems[i].wirings.map(() => 0)
-    }];
+    var maxSingleDistance = value(i, newButtons).map((v, vi) => problems[i].joltages[vi] - v).reduce((a, c) => a > c ? a : c, 0);
 
-    var saw = 0;
+    var minPushes = newButtons.reduce((a, c) => a + c[0], 0) + maxSingleDistance;
 
-    console.log(problems[i].related);
+    if (minPushes >= (best[i] || Number.MAX_SAFE_INTEGER)) {
+        return Number.MAX_SAFE_INTEGER;
+    }
 
-    var solution = Number.MAX_SAFE_INTEGER;
-
-    while (heap.length) {
-        heap.sort((a, b) => b.v - a.v);
-        saw++;
-        var top = heap.pop();
-
-        var topDist = distance(i, top.pushes);
-
-        if (topDist == 0) {
-            //done
-
-            var score = pushCount(top.pushes);
-            if (score < solution) {
-                solution = score;
-                console.log(top.pushes);
+    if (newButtons.every(b => b[0] == b[1])) {
+        // found a solution or dead end
+        if (distance(i, newButtons) == 0) {
+            if (minPushes < (best[i] || Number.MAX_SAFE_INTEGER)) {
+                best[i] = minPushes;
+                console.log(i, minPushes);
             }
-        }
-
-        // find cheap tricks
-
-        var applicableSuggesions = problems[i].related.filter(f => f.relatedButtons.length == 2);
-
-        applicableSuggesions = applicableSuggesions.filter(s => {
-            return top.pushes.reduce((a, c, ci) => { return a + s.relatedButtons.includes(ci) ? c : 0 }, 0) < s.max
-        });
-
-        var buttonMinMaxes = applicableSuggesions.map(as => {
-            var existantClicks = as.relatedButtons.reduce((a, c) => {
-                return a + top.pushes[c];
-            }, 0);
-            return {
-                buttons: as.relatedButtons,
-                count: as.max - existantClicks
-            };
-        });
-
-        function enqueue(pushes) {
-            var dist = distance(i, pushes);
-            var pc = pushCount(pushes);
-            var k = key(value(i, pushes));
-
-            if (dist < topDist && (!seen[k] || pc < seen[k]) && pc < solution) {
-                heap.push({
-                    v: dist,
-                    pushes: pushes
-                });
-                seen[k] = pc;
-            }
-        }
-
-        //console.log('reo', top.pushes, value(i, top.pushes), problems[i], applicableSuggesions, buttonMinMaxes);
-
-        buttonMinMaxes.forEach(mm => {
-            for(var j = 0; j < mm.count; j++) {
-                var jj = mm.count - j;
-
-                var newPushes = clone(top.pushes);
-                newPushes[mm.buttons[0]] += j;
-                newPushes[mm.buttons[1]] += jj;
-
-                //console.log('SC', top.pushes, newPushes);
-
-                enqueue(newPushes);
-            }
-        });
-
-        for(var j = 0; j < top.pushes.length; j++) {
-            var newPushes = clone(top.pushes);
-            newPushes[j]++;
-
-            enqueue(newPushes);
+            return minPushes;
+        } else {
+            return Number.MAX_SAFE_INTEGER;
         }
     }
 
-    console.log('SOLUTION', i, saw)
+    // var selection = newButtons.map((nb, nbi) => { return { i: nbi, v: nb[1]-nb[0] } }).filter(x => x.v > 0);
+    // var bi = selection[Math.floor(Math.random()*selection.length)].i;
 
-    return solution;
+    var bi = 0;
+    newButtons.forEach((nb, nbi) => {
+        var existingDiff = (newButtons[bi][1] - newButtons[bi][0]);
+        var newDiff = (nb[1] - nb[0]);
+        if (existingDiff == 0) {
+            bi = nbi;
+        } else {
+            if (newDiff > 0 && newDiff < existingDiff) {
+                bi = nbi;
+            }
+        }
+    });
+
+    var lowPoint = newButtons[bi][0];
+    var highPoint = newButtons[bi][1];
+    var midPointA = Math.floor((lowPoint + highPoint) / 2)+1;
+    var midPointB = Math.floor((lowPoint + highPoint) / 2);
+
+    if (midPointA > highPoint) {
+        midPointA = highPoint;
+    }
+
+    var mutatedButtonA = newButtons.map(x => x.map(xx => xx));
+    mutatedButtonA[bi][0] = midPointA;
+    var res1 = search(i, mutatedButtonA);
+
+    var mutatedButtonB = newButtons.map(x => x.map(xx => xx));
+    mutatedButtonB[bi][1] = midPointB;
+    var res2 = search(i, mutatedButtonB);
+
+    if (res1 < res2) {
+        return res1;
+    }
+
+    return res2;
 }
 
 var total = 0;
 
+function calculateRanges(i, r) {
+    var ranges = r.map(m => m.map(n => n));
+    var lastHadChanges = true;
+    var lastHadInvalid = false;
+
+    while(lastHadChanges && !lastHadInvalid) {
+        lastHadChanges = false;
+        problems[i].related.forEach(formula => {
+            formula.relatedButtons.forEach((b) => {
+                var nonSelectedButtons = formula.relatedButtons.filter(bb => bb != b);
+                [ranges[b], changed] = shrink(ranges[b], ranges.filter((_, ri) => nonSelectedButtons.includes(ri)), formula.max);
+                if (changed) {
+                    lastHadChanges = changed;
+                }
+            })
+        })
+        lastHadInvalid = ranges.some(r => r[0] > r[1]);
+    }
+    return ranges;
+}
+
 for(var i = 0; i < problems.length; i++) {
-    var v = search(i);
-    console.log(v);
+    cache = {};
+    var buttons = calculateRanges(i, problems[i].wirings.map(() => [0, Number.MAX_SAFE_INTEGER]));
+    console.log('init', i, buttons.length, buttons);
+
+    var v = search(i, buttons);
+    console.log('ANSWER', i, v);
     total += v;
 }
 
